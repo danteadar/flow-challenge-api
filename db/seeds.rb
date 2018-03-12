c5dfec83db7d4e13f3fc4e18b675a22f8c746347
andrew <danteadar@gmail.com>
2018-03-11 20:16:57 -0700

    generated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# user = User.create(
+  name: "example",
+  email: "random@email.com",
+  password: "123123",
+  password_confirmation: "123123"
+  )
+user = User.create(
+  name: "example2",
+  email: "random2@email.com",
+  password: "123123",
+  password_confirmation: "123123"
+  )
+
+invitation = Invitation.create(
+  sender: "example",
+  recipient: "example2",
+  message: "Please Accept",
+  url: "this url intentionally left blank"
+  )
+
+invitation = Invitation.create(
+  sender: "example",
+  recipient: "example3",
+  message: "Please Accept",
+  url: "this url intentionally left blank",
+  status: "2018-03-12 02:56:31.841938"
+  )
+
